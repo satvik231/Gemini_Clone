ff521c21716216834029f864615e44087c7effe9
Satvik <satvikkamble95@gmail.com>
2025-03-08 18:33:24 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import Main from "./components/Main/Main";
+import Sidebar from "./components/SideBar/Sidebar";
+const App = () => {
+  return (
+    <>
+      <Sidebar />
+      <Main />
+    </>
+  );
+};
+export default App;
